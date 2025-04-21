d5cd4eaba2d959ed56e567b1eb4aa84a37339b81
Sushanth N <sushanthnandeti96@gmail.com>
2025-04-20 23:24:37 -0400

    Styling added for the Products list page

--- components/product-card.tsx
+++ components/product-card.tsx
@@ -1,6 +1,7 @@
 import Stripe from "stripe";
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 import Image from "next/image";
+import { Button } from "./ui/button";
 
 interface Props {
     product : Stripe.Product;
@@ -9,24 +10,29 @@ interface Props {
 export const ProductCard = ({product} : Props) => {
     
     const price = product.default_price as Stripe.Price;
-    return <Card className="relative overflow-hidden rounded-lg shadow-md border-gray-300"> {product.images && product.images[0] &&  (
-            <div className="relative h-80 w-full">
+
+    return <Card className="group hover:shadow-2xl transition duration-300 py-0 h-full flex flex-col border-gray-300 gap-0"> 
+            {product.images && product.images[0] &&  (
+            <div className="relative h-60 w-full">
                 <Image 
                     alt={product.name} 
                     src={product.images[0]}   
                     layout = "fill"
                     objectFit = "cover"
+                    className="group-hover:opacity-90 transition-opacity duration-300 rounded-t-lg"
                    />
             </div>
         )}  
-            <CardHeader>
+            <CardHeader className="p-4">
             
-                    <CardTitle className="text-3xl font-bold text-white mb-2"> 
+                    <CardTitle className="text-xl font-bold text-gray-800"> 
                         {product.name}
                     </CardTitle>
 
-                    <CardContent>{price && price.unit_amount && 
-                        <p className="text-xl"> ${(price.unit_amount / 100).toFixed(2)}</p>}
+                    <CardContent className="p-4 flex-grow flex flex-col justify-between">
+                        {price && price.unit_amount && 
+                        <p className="text-lg font-semibold text-gray-900"> ${(price.unit_amount / 100).toFixed(2)}</p>}
+                        <Button className="mt-4 bg-black text-white"> View Details</Button>
                     </CardContent>
         
             </CardHeader>
--- components/product-list.tsx
+++ components/product-list.tsx
@@ -9,11 +9,15 @@ interface Props {
 export const ProductList = ( {products} : Props) => {
     
     return ( 
-        <div>
-            {/* <div>
-                <input type="text" placeholder="Seach products here ..."> </input>
-            </div> */}
-            <ul>
+        <div >
+            <div className="mb-6 flex justify-center">
+                <input 
+                    type="text" 
+                    placeholder="Seach products here ..."
+                    className="w-full max-w-md rounded border border-gray-300 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"> 
+                </input>
+            </div>
+            <ul className="mt-6 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
                {products.map((product, key) => {
                     return (
                         <li key={key}> <ProductCard  product={product} /></li>

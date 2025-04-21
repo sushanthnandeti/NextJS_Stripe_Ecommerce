79c4de6ba32552b42de90eacdd0f3100c6619841
Sushanth N <sushanthnandeti96@gmail.com>
2025-04-21 01:31:57 -0400

    Search functionality added for the products page

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -1,5 +1,8 @@
+'use client'
+
 import Stripe from "stripe"
 import { ProductCard } from "./product-card"
+import { useState } from "react"
 
 interface Props {
     products : Stripe.Product[]
@@ -7,18 +10,30 @@ interface Props {
 
 
 export const ProductList = ( {products} : Props) => {
+
+    const [searchTerm, setSearchTerm] = useState<string>("");
+
+    const filteredProducts = products.filter((product) => {
+        const term = searchTerm.toLowerCase();
+        const nameMatch = product.name.toLowerCase().includes(term);
+        const descriptionMatch = product.description ? product.description?.toLowerCase().includes(term) : false
+
+        return nameMatch || descriptionMatch
+    })
     
     return ( 
         <div >
             <div className="mb-6 flex justify-center">
                 <input 
                     type="text" 
+                    value = {searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
                     placeholder="Seach products here ..."
                     className="w-full max-w-md rounded border border-gray-300 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"> 
                 </input>
             </div>
             <ul className="mt-6 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-               {products.map((product, key) => {
+               {filteredProducts.map((product, key) => {
                     return (
                         <li key={key}> <ProductCard  product={product} /></li>
                     )

51c6429818f1db0b2502cc008761bf3c6353e9db
Sushanth N <sushanthnandeti96@gmail.com>
2025-04-21 01:13:55 -0400

    Products main page layout change

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -9,5 +9,10 @@ export default async function ProductsPage() {
       expand : ["data.default_price"],
     })
 
-    return <ProductList products = {products.data}/ >
+    return <div className="pb-8"> 
+        <h1 className="text-3xl font-bold leading-none tracking-tight text-foreground text-center mb-8">
+          All Products
+       </h1>
+          <ProductList products = {products.data} />
+      </div>
 }

78e93a239184ff83c68ddfeaeb588ebe978ca4d7
Sushanth N <sushanthnandeti96@gmail.com>
2025-04-24 20:21:08 -0400

    Zustand setup and cartStore hook created

---
+++ store/cart-store.ts
@@ -0,0 +1,39 @@
+import {create} from "zustand";
+import {persist} from "zustand/middleware";
+
+export interface cartItem {
+    id: string;
+    name: string;
+    price: number;
+    quantity: number;
+    image: string;
+}
+
+interface CartStore {
+    items: cartItem[];
+    addItem: (data: cartItem) => void;
+}
+
+
+export const useCartStore = create<CartStore>() (
+    persist((set) => ({
+        items: [],
+        addItem : (item) => set((state) => {
+                const existing = state.items.find((i) => i.id === item.id);
+                if (existing) {
+                    return {
+                        items: state.items.map((i) => 
+                            i.id === item.id ? {...i, quantity: i.quantity + item.quantity} : i    
+                        )
+                    }
+                }
+
+                return {
+                    items : [...state.items, item]
+                }
+
+            })
+        }), {name : "cart"})
+    
+)
+       

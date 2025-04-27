3833869b4dd84dc11a107b88094399f3ad72e342
Sushanth N <sushanthnandeti96@gmail.com>
2025-04-26 21:54:14 -0400

    add and remove buttons added for products

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,11 +1,12 @@
 'use client'
 
+import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { useCartStore } from "@/store/cart-store"
 
 export default function CheckoutPage () {
 
-    const {items} = useCartStore();
+    const {items, addItem, removeItem} = useCartStore();
 
     const price = items.reduce((acc, item) => acc + item.price * item.quantity, 0)
 
@@ -25,10 +26,17 @@ export default function CheckoutPage () {
                     <ul>
                         {items.map((item,key) => (
                             <li key={key}>
-                                <span> {item.name}</span>
-                                <span> ${((item.price * item.quantity)/100).toFixed(2)}</span>
+                                <div>
+                                    <span> {item.name}</span>
+                                    <span> ${((item.price * item.quantity)/100).toFixed(2)}</span>
+                                </div>
+                                <div>
+                                <Button variant="outline" onClick = {()=>removeItem(item.id)}> - </Button>
+                                <span className="text-lg font-semibold">{item.quantity}</span>
+                                <Button onClick={() => addItem({...item, quantity : 1})}>+</Button>
+                                </div>
                             </li>
-                        ))}
+                        ))} 
                     </ul>
                     </CardContent>
                 </Card>
